b74d065c3c3285766d8297572a407530b4f963b2
Andraž De Luisa <andrazdl@gmail.com>
2017-11-08 18:47:28 +0100

    izpis csv

---
+++ projekt.py
@@ -0,0 +1,142 @@
+import requests
+import re
+import os
+import csv
+
+
+
+
+salaries_link = 'http://www.spotrac.com/nba/rankings/2016/cap-hits/'
+stats_link = 'https://www.basketball-reference.com/leagues/NBA_2017_per_game.html'
+directory = 'web_pages'
+salaries_basename = 'salaries.html'
+stats_basename = 'stats.html'
+salaries_filename = os.path.join(directory, salaries_basename)
+stats_filename = os.path.join(directory, stats_basename)
+salaries_csv_filename = 'salaries.csv'
+stats_csv_filename = 'stats.csv'
+
+
+def download_url_to_string(url):
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException:
+        print('failed to connect to url' + url)
+        return
+    pagecontent = r.text
+    return pagecontent
+
+def save_string_to_file(text, filename):
+    with open(filename, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return 
+
+def save_frontpage_to_file(url, filename):
+    pagecontent = download_url_to_string(url)
+    save_string_to_file(pagecontent, filename)
+    return
+
+def read_file_to_string(filename):
+    with open(filename, 'r', encoding='utf8') as file_in:
+        text = file_in.read()
+    return text
+
+#extracting players stats
+
+def page_to_players_stats(page):
+    rx = re.compile(r'<tr class="full_table" >(.*?)</tr>',
+                    re.DOTALL)
+    players = re.findall(rx, page)
+    return players
+
+def get_dict_from_player_stats_block(block):
+    rx = re.compile(r'>(?P<index>.*?)<.*html">'
+                    r'(?P<name>.*?)<.*stat="pos" >'
+                    r'(?P<position>.*?)<.*stat="age" >'
+                    r'(?P<age>.*?)<.*stat="team_id" >(<a .*html">)?'
+                    r'(?P<team>.*?)<.*stat="g" >'
+                    r'(?P<games>.*?)<.*stat="gs" >'
+                    r'(?P<g_started>.*?)<.*stat="mp_per_g" >'
+                    r'(?P<minutes>.*?)<.*stat="fg_per_g" >'
+                    r'(?P<fg>.*?)<.*stat="fga_per_g" >'
+                    r'(?P<fga>.*?)<.*stat="fg_pct" >'
+                    r'(?P<fg_pct>.*?)<.*stat="ft_per_g" >'
+                    r'(?P<ft>.*?)<.*stat="fta_per_g" >'
+                    r'(?P<fta>.*?)<.*stat="ft_pct" >'
+                    r'(?P<ft_pct>.*?)<.*stat="orb_per_g" >'
+                    r'(?P<oreb>.*?)<.*stat="drb_per_g" >'
+                    r'(?P<dreb>.*?)<.*stat="trb_per_g" >'
+                    r'(?P<totreb>.*?)<.*stat="ast_per_g" >'
+                    r'(?P<assists>.*?)<.*stat="stl_per_g" >'
+                    r'(?P<steals>.*?)<.*stat="blk_per_g" >'
+                    r'(?P<blocks>.*?)<.*stat="tov_per_g" >'
+                    r'(?P<turnovers>.*?)<.*stat="pf_per_g" >'
+                    r'(?P<pfouls>.*?)<.*stat="pts_per_g" >'
+                    r'(?P<points>.*?)</td>',
+                    re.DOTALL)
+    data = re.search(rx, block)
+    player_stats_dict = data.groupdict()
+    return player_stats_dict
+
+def players_stats_from_file(filename):
+    page = read_file_to_string(filename)
+    blocks = page_to_players_stats(page)
+    players = [get_dict_from_player_stats_block(block) for block in blocks]
+    return players
+
+#exctracting players salaries
+
+def page_to_players_salaries(page):
+    rx = re.compile(r'<td class="rank-name player noborderright"'
+                    r'(.*?)\s</span></td>',
+                    re.DOTALL)
+    players = re.findall(rx, page)
+    return players
+
+def get_dict_from_player_salary_block(block):
+    rx = re.compile(r'class="team-name">'
+                    r'(?P<name>.*?)<.*title="(Reserve Suspended)?">'
+                    r'(?P<salary>.*?)\s',
+                    re.DOTALL)
+    data = re.search(rx, block)
+    player_salary_dict = data.groupdict()
+    return player_salary_dict
+
+def players_salaries_from_file(filename):
+    page = read_file_to_string(filename)
+    blocks = page_to_players_salaries(page)
+    players = [get_dict_from_player_salary_block(block) for block in blocks]
+    return players
+    
+#writing csv
+
+def write_csv(fieldnames, rows, filename):
+    with open(filename, 'w', encoding='utf8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+    return
+
+def write_players_to_csv(players, filename):
+    write_csv(players[0].keys(), players, filename)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
